2739f9f530c61fc8a2786831bc3526c688ad7368
BrunoUemura <bruno.h.uemura@outlook.com>
2022-03-29 00:44:28 -0300

    finish banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,11 +1,11 @@
 import styles from "./styles.module.scss";
-import bannerImage from "../../assets/banner-image1.png";
+import bannerImage from "../../assets/banner-image3.png";
 
 export default function Banner() {
   return (
     <div className={styles.banner}>
       <div className={styles.information}>
-        <span>Hi, I am</span>
+        <span>Hi there 🖐️, I am</span>
         <span className={styles.name}>Bruno Hideki Uemura</span>
         <span>a Software Engineer</span>
         <a href="#about" className={styles.button}>
