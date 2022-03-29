df97c857e6cfb5fd154f51f639be053d48921adc
BrunoUemura <bruno.h.uemura@outlook.com>
2022-03-28 22:59:25 -0300

    add banner initial design

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,20 @@
+import styles from "./styles.module.scss";
+import bannerImage from "../../assets/banner-image1.png";
+
 export default function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className={styles.banner}>
+      <div className={styles.information}>
+        <span>Hi, I am</span>
+        <span className={styles.name}>Bruno Hideki Uemura</span>
+        <span>a Software Engineer</span>
+        <a href="#about" className={styles.button}>
+          See more
+        </a>
+      </div>
+      <div className={styles.image}>
+        <img src={bannerImage} alt="Banner Image" />
+      </div>
+    </div>
+  );
 }
