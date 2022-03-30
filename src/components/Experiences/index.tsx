cdb1d2d532792c17a961ce1d2506b194f2670d40
BrunoUemura <bruno.h.uemura@outlook.com>
2022-03-30 02:20:04 -0300

    add experiences

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { MONTHS } from "../../util/enums";
 import { jobs } from "../../util/jobs";
 import styles from "./styles.module.scss";
 
 export default function Experiences() {
+  const [selectedJob, setSelectedJob] = useState<number>(0);
+
   const formatDate = (date: string) => {
     const convertedDate = new Date(date);
     const monthNumber = convertedDate.getMonth();
@@ -24,30 +27,50 @@ export default function Experiences() {
 
     if (result < 12) {
       return `${result}m`;
-    } else {
-      const years = Math.floor(result / 12);
-      const months = result - 12;
-      return `${years}y ${months}m`;
     }
+
+    const years = Math.floor(result / 12);
+    const months = result - 12;
+    return `${years}y ${months}m`;
   };
 
   return (
     <div id="experiences" className={styles.experiences}>
-      <div className={styles.jobs}>
-        {jobs.map((job, index) => (
-          <div key={index} className={styles.job}>
-            <img src={job.image} alt={`${job.companyName} logo`} />
-            <div className={styles.jobDetail}>
-              <span className={styles.companyName}>{job.companyName}</span>
-              <span className={styles.jobTitle}>{job.title}</span>
-              <span className={styles.jobDate}>
-                {formatDate(job.startDate)} -{" "}
-                {job.endDate !== null ? formatDate(job.endDate) : "Current"} •{" "}
-                {calculateMonths(job.startDate, job.endDate)}
-              </span>
+      <div className={styles.title}>
+        <span>Experiences</span>
+      </div>
+      <div className={styles.details}>
+        <div className={styles.jobs}>
+          {jobs.map((job, index) => (
+            <div
+              key={index}
+              className={styles.job}
+              onClick={() => setSelectedJob(index)}
+            >
+              <img src={job.image} alt={`${job.companyName} logo`} />
+              <div className={styles.jobInformation}>
+                <span className={styles.companyName}>{job.companyName}</span>
+                <span className={styles.jobTitle}>{job.title}</span>
+                <span className={styles.jobDate}>
+                  {formatDate(job.startDate)} -{" "}
+                  {job.endDate !== null ? formatDate(job.endDate) : "Current"} •{" "}
+                  {calculateMonths(job.startDate, job.endDate)}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className={styles.jobDetail}>
+          <span className={styles.companyName}>
+            {jobs[selectedJob].companyName}
+          </span>
+          <span className={styles.jobTitle}>{jobs[selectedJob].title}</span>
+          <span className={styles.jobDescription}>
+            {jobs[selectedJob].description.map((text) => (
+              <p>{text}</p>
+            ))}
+          </span>
+        </div>
       </div>
     </div>
   );
